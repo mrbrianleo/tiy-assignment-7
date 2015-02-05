4c0503b6527bcb808b34d27aa3b59515c0049c86
Brian Leonard <brian@theleoreport.com>
2015-02-05 16:32:51 -0500

    question 2 - complete

--- answers.js
+++ answers.js
@@ -1,9 +1,9 @@
-//Questions
+// Questions
 //
-//1.
-//Show me how to calculate the average price of all items. Please console.log the average.
-//The average price is $23.63
-//I'll accept either $23.63 or $23.62
+// 1.
+// Show me how to calculate the average price of all items. Please console.log the average.
+// The average price is $23.63
+// I'll accept either $23.63 or $23.62
 //
 
 //need to get an array of all prices
@@ -20,8 +20,28 @@ prices.length;
 var avgPrice = sumPrices / prices.length;
   console.log("The average price is" + " " + "$" + avgPrice);
 
+//2.
+// Show me how to get an array of items that cost between $14.00 and $18.00 USD
+// "Items that cost between $14.00 USD and $18.00 USD:"
+// [
+// {
+//   title: "1970s Coors Banquet Glass Beer Pitcher",
+//   ...
+// },
+// {
+//   title: "The Three Broomsticks Customizable Beer Stein Mug, Harry Potter Inspired, hogsmeade village, harry potter gift, three broomsticks mug",
+//   ...
+// },
+// {
+//   title: "Hand Painted Colorful Feather Glass",
+//   ...
+// }
+// ]
 
- 
+//use filter to get the cost of items between
+var costBetween = items.filter(function(item){
+  return item.price >= 14 && item.price <= 18;
+});
 
 
 
