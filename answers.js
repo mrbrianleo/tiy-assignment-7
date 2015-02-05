938781e2894ea6d864f6118e21a84f4136ff2c3e
Brian Leonard <brian@theleoreport.com>
2015-02-05 15:16:28 -0500

    question 1 - get prices array

---
+++ answers.js
@@ -0,0 +1,26 @@
+//Questions
+//
+//1.
+//Show me how to calculate the average price of all items. Please console.log the average.
+//The average price is $23.63
+//I'll accept either $23.63 or $23.62
+//
+
+//need to get an array of all prices
+var prices = items.map(function(item) {
+  return item.price;
+});
+//sum of prices array
+
+//need to get count of all prices
+prices.length;
+//divide sum of prices array by count of prices
+
+
+
+ 
+
+
+
+
+
