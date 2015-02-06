51d18f521c7148382e456d1907b14c0968792b21
Brian Leonard <brian@theleoreport.com>
2015-02-05 22:23:28 -0500

    excercises.js 3 complete

--- exercises.js
+++ exercises.js
@@ -35,7 +35,12 @@ function maxOfThree(a, b, c){
 // ---------------------
 
 function isVowel(char){
-    //...
+  //...
+  if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // ---------------------
